c462466083d5c7df78e6751b1c2fb39192deba06
dluman <dluman@allegheny.edu>
2020-12-02 09:45:16 -0500

    Professor is an idiot

--- Images/grid.py
+++ Images/grid.py
@@ -6,9 +6,9 @@ files = os.listdir("squares")
 
 files.sort()
 
-files = [file for file in files if not file.startswith(".ipynb")]
+files = [file for file in files if file.endswith(".png")]
 
-composite = Image.new("RGB",(300,300))
+composite = Image.new("RGB",(300,300)) # <-- this represents the final image
 
 files = files[0:9]
 
@@ -16,12 +16,12 @@ x = 0
 y = 0
 
 for square in files:
-    if x % 3 == 0 and x != 0:
+    if x % 3 == 0 and x != 0: # <-- change the 3 to however many imgs per row
         x = 0
         y += 1
     sq = Image.open(f"squares/{square}")
     print(f"Opening {square}.")
-    composite.paste(sq,(x*100,y*100))
+    composite.paste(sq,(x*100,y*100)) # <-- change the 100s depending on your img size
     x += 1
 
 composite.save("composite.png")
